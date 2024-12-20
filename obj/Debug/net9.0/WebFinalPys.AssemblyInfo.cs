3fdeb9fdef4637c00ef32d277fe8761c3da02859
VipOzGur <vipozgurozkan@gmail.com>
2024-12-20 22:23:15 +0300

    clone remote main from local main

--- obj/Debug/net9.0/WebFinalPys.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebFinalPys.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebFinalPys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e03c374c7b3ee7aebce377d168c6fe281c10fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e5a7196fb84b0e30a7c22ac8637694836505f23")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebFinalPys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebFinalPys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
